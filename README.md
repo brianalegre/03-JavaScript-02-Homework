8b939655b902a2d174ad89c7ed240ebbe9d6c99c
Brian Alegre <brialegre@yahoo.com>
2022-04-03 19:52:11 -0700

    Updated README, Added Variables, Added Password Length + Check

--- README.md
+++ README.md
@@ -20,13 +20,13 @@ window.alert(message);
 - Broswer Prompts
 - Message needs to be in ""
 - Gives only an OK button
-- - Example: window.alert("Hi There");
+  - Example: window.alert("Hi There");
 
 alert(message);
 - Link: https://developer.mozilla.org/en-US/docs/Web/API/Window/alert
 - Alert that doesn't require a response
 - Gives only an OK button
-- - Exampe: alert("Hello");
+  - Exampe: alert("Hello");
 
 
 window.confirm(message)
@@ -34,11 +34,23 @@ window.confirm(message)
 - Returns a boolean, TRUE or FALSE
 - True - OK
 - False - Cancel
-- - Example: window.confirm("Click OK to Leave or Cancel to stay");
+  - Example: window.confirm("Click OK to Leave or Cancel to stay");
 
 
 window.prompt(message, default)
 - Link: https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
 - Asks for user input
 - if default is not used, value is assigned as Undefined 
-- - Example: window.prompt("What is your name", 'enter your name');
+  - Example: window.prompt("What is your name", 'enter your name');
+
+
+var generateBtn = document.querySelector("#generate");
+- Link: https://www.youtube.com/watch?v=3oOKAJTD2F8
+- var generateBtn
+  - Create new variable generateBtn
+-document.querySelector("#generate")
+  - selects the line of code with the ID of generate
+  - document. can be changed to tag names to target specify more specificity
+
+.addEventListener()
+- Link: https://www.youtube.com/watch?v=XF1_MlZ5l6M
--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// Variables
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "j", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbol = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "~", "`", ";", ":", "'", ",", ".", "/", "?", ">", "<", "|", "]", "[", "{", "}"];
+
+
+// Print Variables to console
+console.log(lowerCase);
+console.log(upperCase);
+console.log(number);
+console.log(symbol);
+
+
+// Ask user for password length
+var passwordLength = window.prompt("Enter desired password length, must be at least 8 characters long, and no more than 128 characters")
+
+// Check valid password lenth
+if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("Enter valid password length.  Please start over.")
+}
+
+// Print Variable to console
+console.log(passwordLength);
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -14,25 +40,26 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// Broswer Prompts
-// Needs message to be in ""
-// Gives only an OK button
-window.alert("Hi there");
+// // Broswer Prompts
+// // Needs message to be in ""
+// // Gives only an OK button
+// window.alert("Hi there");
 
-// Alert that doesn't require a response
-// Gives only an OK button
-alert("Hello");
+// // Alert that doesn't require a response
+// // Gives only an OK button
+// alert("Hello");
 
-// Returns a boolean, TRUE or FALSE
-// True - OK
-// False - Cancel
-window.confirm("Click OK to Leave or Cancel to stay");
+// // Returns a boolean, TRUE or FALSE
+// // True - OK
+// // False - Cancel
+// window.confirm("Click OK to Leave or Cancel to stay");
 
 
-// Asks for user input
-// if default is not used, value is assigned as Undefined 
-//  ** window.prompt(message, default) **
-window.prompt("What is your name", 'enter your name');
+// // Asks for user input
+// // if default is not used, value is assigned as Undefined 
+// //  ** window.prompt(message, default) **
+// window.prompt("What is your name", 'enter your name');
 
+// console.log(generateBtn);
 
 
